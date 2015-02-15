3ac1e0c75553da64417ee18742954d6f874ddd7a
samsung <samsung@abhi>
2015-02-16 04:03:40 +0530

    working fine but still have to add a new feature of "show stop hotspot" if hotspot is already running in background

---
+++ src/aahowifi/AAhowifi.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aahowifi;
+import javax.swing.JFrame;
+/**
+ *
+ * @author samsung
+ */
+public class AAhowifi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     aahoGUI a = new aahoGUI();
+     a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    a.setVisible(true);// TODO code application logic here
+    }
+    
+}
